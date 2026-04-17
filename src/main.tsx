58c39375e9a6a3d017a617171dba1fead1ee976d
celsowm <celsowm@gmail.com>
2026-04-17 18:57:29 -0300

    fix: make pages routing work on github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import App from './app/App';
 import { appTheme } from './app/theme';
@@ -12,9 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <ThemeProvider theme={appTheme}>
       <CssBaseline />
       <I18nProvider>
-        <BrowserRouter basename="/ai-llm-course">
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </I18nProvider>
     </ThemeProvider>
   </React.StrictMode>,
